32655081f1e0d432ba5940a5c9ae7719ec466aff
Mikhail Tortsev <59667317+flashm123@users.noreply.github.com>
2021-11-01 11:35:31 +0300

    Add 30 sec pause

--- test_items.py
+++ test_items.py
@@ -6,6 +6,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_find_add_to_basket_button(browser):
     browser.get(link)
+    time.sleep(30)
     find_buttons = browser.find_elements_by_css_selector('#add_to_basket_form .btn-add-to-basket')
     assert len(find_buttons) > 0, 'Не найдена кнопка добавления в корзину'
     assert len(find_buttons) < 2, 'Кнопка добавления в корзину не уникальна'
